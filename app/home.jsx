b2e55194bffcce48d7627ab46c2eae8f6056c74b
Trevor_143 <trevorarapu2@gmail.com>
2023-11-23 17:44:33 +0300

    done with welcom slider

---
+++ app/home.jsx
@@ -0,0 +1,47 @@
+import { View, Text, Image } from 'react-native'
+import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { StatusBar } from 'expo-status-bar'
+import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import ImageSlider from '../components/ImageSlider';
+
+export default function Home() {
+  return (
+    <SafeAreaView className="flex-1 bg-white flex space-y-5" edges={['top']}>
+      <StatusBar style='dark' />
+      <View className="flex-row justify-between items-center mx-5">
+        <View className="space-y-2">
+            <Text
+                style={{ fontSize: hp(4.5) }}
+                className="font-bold tracking-wider text-neutral-700 h-9"
+            >
+                Good Afternoon
+            </Text>
+            <Text
+                style={{ fontSize: hp(2.5) }}
+                className=" font-black tracking-wider text-red-800 "
+            >
+                Arapu Trevor
+            </Text>
+        </View>
+        <View className="flex justify-center items-center space-y-2">
+            <Image 
+                source={require('../assets/images/user.webp')}
+                style={{ height: hp(6), width: hp(6) }}
+                className="rounded-full"
+            />
+            <View 
+                className="bg-neutral-200 rounded-full flex justify-center items-center border-[3px] border-neutral-300 "
+                style={{ height: hp(5.5), width: hp(5.5) }}
+            >
+                <Ionicons name="notifications" size={hp(3)} color="grey" />
+            </View>
+        </View>
+      </View>
+      <View>
+        <ImageSlider />
+      </View>
+    </SafeAreaView>
+  )
+}
---
+++ constants/index.js
@@ -0,0 +1,12 @@
+export const sliderImages = [
+    require('../assets/images/slider/christ-db2.png'),
+    require('../assets/images/slider/christ-db17.png'),
+    require('../assets/images/slider/christ-db28.png'),
+    require('../assets/images/slider/christ-db38.png'),
+    require('../assets/images/slider/christ-db40.png'),
+    require('../assets/images/slider/christ-db50.png'),
+    require('../assets/images/slider/christ-wd14.png'),
+    require('../assets/images/slider/christ-wd16.png'),
+    require('../assets/images/slider/christ-wd17.png'),
+    require('../assets/images/slider/christ-wd18.png')
+]
